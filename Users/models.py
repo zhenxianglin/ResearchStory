2acc53884b2aeddbd192f6f5ea94e52c307234cd
blastxiaol <blastxiaol@gmail.com>
2021-07-14 02:28:31 +0800

    user login register logout

--- Users/models.py
+++ Users/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as uget
 
 
-class Account(models.Model):
-    username = models.CharField("username", max_length=10, default="Null")
-    age = models.IntegerField("age", default=-1)
+class User(models.Model):
+    username = models.CharField("username", max_length=10, unique=True, default="Null")
+    age = models.IntegerField("age", null=True)
     gender = models.CharField("gender", max_length=1, choices=(('M', 'Male'), ('F', 'Female')),
                               blank=True, null=True)
-    last_name = models.CharField("Last Name", max_length=50, default="Null")
-    first_name = models.CharField("First Name", max_length=50, default="Null")
-    e_mail = models.CharField("e-mail", max_length=100, default="Null")
-    password = models.CharField(uget("password"), max_length=128, default="Null")
+    last_name = models.CharField("Last Name", max_length=50)
+    first_name = models.CharField("First Name", max_length=50)
+    e_mail = models.CharField("e-mail", max_length=100, unique=True)
+    password = models.CharField("password", max_length=128, unique=True)
+    ticket = models.CharField("ticket", max_length=30, null=True)
 
     def __str__(self):
         return self.username
+
+    class Meta:
+        verbose_name_plural = "Users"
