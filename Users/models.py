5153a54d560bd2991eb7fe8ef43451be0a7c8bcd
blastxiaol <blastxiaol@gmail.com>
2021-07-14 00:31:43 +0800

    user database

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as uget
 
-# Create your models here.
+
+class Account(models.Model):
+    username = models.CharField("username", max_length=10, default="Null")
+    age = models.IntegerField("age", default=-1)
+    gender = models.CharField("gender", max_length=1, choices=(('M', 'Male'), ('F', 'Female')),
+                              blank=True, null=True)
+    last_name = models.CharField("Last Name", max_length=50, default="Null")
+    first_name = models.CharField("First Name", max_length=50, default="Null")
+    e_mail = models.CharField("e-mail", max_length=100, default="Null")
+    password = models.CharField(uget("password"), max_length=128, default="Null")
+
+    def __str__(self):
+        return self.username
