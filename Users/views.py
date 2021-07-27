b81fd7d31f36adfa6df459a96becb08ee495ba0a
Hankin72 <tracyguoji@gmail.com>
2021-07-28 05:02:25 +0800

    asdasdas

--- Users/views.py
+++ Users/views.py
@@ -45,49 +45,99 @@ def register(request):
     context = {"form": form}
     return render(request, 'Users/register.html', context)
 
-
-@login_required
+@login_required(login_url='/userprofile/login/')
 def profile_edit(request, id):
     user = User.objects.get(id=id)
+
+    # 旧教程代码
     # profile = Profile.objects.get(user_id=id)
+    # 新教程代码： 获取 Profile
     if Profile.objects.filter(user_id=id).exists():
+        # user_id 是 OneToOneField 自动生成的字段
         profile = Profile.objects.get(user_id=id)
     else:
         profile = Profile.objects.create(user=user)
 
+
     if request.method == 'POST':
+        # 验证修改数据者，是否为用户本人
         if request.user != user:
-            return HttpResponse("You do not have permission to modify this user information.")
+            return HttpResponse("You do not have permission to modify this user information.。")
+
         # 上传的文件保存在 request.FILES 中，通过参数传递给表单类
         profile_form = ProfileForm(request.POST, request.FILES)
 
         if profile_form.is_valid():
             # 取得清洗后的合法数据
-
             profile_cd = profile_form.cleaned_data
-
-            # profile.age = profile_cd['age']
-            # profile.gender = profile_cd['gender']
-            # profile.last_name = profile_cd['last_name']
-            # profile.first_name = profile_cd['first_name']
+            profile.age = profile_cd['age']
+            profile.gender = profile_cd['gender']
+            profile.last_name = profile_cd['last_name']
+            profile.first_name = profile_cd['first_name']
             profile.bio = profile_cd['bio']
 
-            # # 如果 request.FILES 存在文件，则保存
+            # 如果 request.FILES 存在文件，则保存
             if 'avatar' in request.FILES:
-                profile.avatar = profile_cd['avatar']
+                profile.avatar = profile_cd["avatar"]
 
             profile.save()
-            return redirect("Users:edit", id=id)
+            # 带参数的 redirect()
+            return redirect("userprofile:edit", id=id)
         else:
             return HttpResponse("The registration form is entered incorrectly. Please re-enter~")
 
     elif request.method == 'GET':
         profile_form = ProfileForm()
-        context = {
-            'profile_form': profile_form,
-            'profile': profile,
-            'user': user,
-        }
+        context = { 'profile_form': profile_form, 'profile': profile, 'user': user }
         return render(request, 'Users/edit.html', context)
     else:
         return HttpResponse("Please use GET or POST to request data.")
+
+
+
+#
+# @login_required
+# def profile_edit(request, id):
+#     user = User.objects.get(id=id)
+#     # profile = Profile.objects.get(user_id=id)
+#     if Profile.objects.filter(user_id=id).exists():
+#         profile = Profile.objects.get(user_id=id)
+#     else:
+#         profile = Profile.objects.create(user=user)
+#
+#     if request.method == 'POST':
+#         if request.user != user:
+#             return HttpResponse("You do not have permission to modify this user information.")
+#         # 上传的文件保存在 request.FILES 中，通过参数传递给表单类
+#         profile_form = ProfileForm(request.POST, request.FILES)
+#
+#         if profile_form.is_valid():
+#             # 取得清洗后的合法数据
+#
+#             profile_cd = profile_form.cleaned_data
+#
+#             # profile.age = profile_cd['age']
+#             # profile.gender = profile_cd['gender']
+#             # profile.last_name = profile_cd['last_name']
+#             # profile.first_name = profile_cd['first_name']
+#             profile.bio = profile_cd['bio']
+#
+#             # # 如果 request.FILES 存在文件，则保存
+#             if 'avatar' in request.FILES:
+#                 profile.avatar = profile_cd['avatar']
+#
+#             profile.save()
+#             return HttpResponseRedirect(reverse('Users:edit',args=[id]))
+#         else:
+#             return HttpResponse("The registration form is entered incorrectly. Please re-enter~")
+#
+#     elif request.method == 'GET':
+#         profile_form = ProfileForm()
+#         context = {
+#             'profile_form': profile_form,
+#             'profile': profile,
+#             'user': user,
+#         }
+#         return render(request, 'Users/edit.html', context)
+#     else:
+#         return HttpResponse("Please use GET or POST to request data.")
