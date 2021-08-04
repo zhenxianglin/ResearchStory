4bc32b7e740c2a55da88acad20d1fdfa8c7f5fa7
blastxiaol <blastxiaol@gmail.com>
2021-08-04 22:58:54 +0800

    update

--- forum/views.py
+++ forum/views.py
@@ -9,13 +9,14 @@ from forum.models import Forum
 from forum.ForumForm import ForumForm
 
 
-
 def forumList(request):
     if request.method == "GET":
         forum = Forum.objects.filter().order_by('created_time')
-        return render(request, 'forum_list.html', locals())
+        kwargs = {"forum": forum, }
+        return render(request, 'forum/forum_list.html', kwargs)
 
-def post_forum(request,parent_forum_id=None):
+
+def post_forum(request, parent_forum_id=None):
     if request.method == 'POST':
         forum = ForumForm(request.POST)
 
@@ -23,21 +24,22 @@ def post_forum(request,parent_forum_id=None):
             new_forum=forum.save(commit=False)
             new_forum.author=request.author
 
-            #second
+            # second
             if parent_forum_id:
-                parent_forum=Forum.objects.get(id=parent_forum_id)
+                parent_forum = Forum.objects.get(id=parent_forum_id)
                 # 如果层级超过二级，则自动转为二级
                 new_forum.parent_id=parent_forum.get_root().id
-                #被回复人
+                # 被回复人
                 new_forum.reply_to=parent_forum.author
                 new_forum.save()
                 return HttpResponse("200 OK")
             new_forum.save()
-            return HttpResponseRedirect(reverse("forum:forumList"))#redirect(story)
+            return HttpResponseRedirect(reverse("forum:forumList"))  # redirect(story)
 
         else:
             return HttpResponse('There is something wrong with this form. Please fill it out again. ')
 
-    elif request.method=='GET':
-        forum=ForumForm()
-        return render(request,'forum/forum_reply.html',locals())
+    elif request.method == 'GET':
+        forum = ForumForm()
+        kwargs = {"forum": forum}
+        return render(request, 'forum/forum_reply.html', kwargs)
