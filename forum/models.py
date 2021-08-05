1a9cec359295ce9c38dbdaad9889611e179eb69a
Tangmengqi428 <874381226@qq.com>
2021-08-05 16:55:20 +0800

    can open forum after inserting a data

--- forum/models.py
+++ forum/models.py
@@ -12,12 +12,12 @@ from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
 class Forum(MPTTModel):
-    title=models.CharField(verbose_name="title", max_length=200)
+    title=models.CharField(verbose_name="title", max_length=200,null=True)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     created_time=models.DateTimeField(verbose_name='created_time', auto_now=True)
     text=RichTextField()
-    parent_comment = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='comm_par')
-    reply_to_user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='rep_to_user')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='comm_par')
+    reply_to = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE, related_name='rep_to_user')
     views=models.PositiveIntegerField(verbose_name='views', default=0, auto_created=True)
 
     class MPTTMeta:
