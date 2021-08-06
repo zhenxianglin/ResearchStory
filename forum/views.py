003873878aa4aadc53d927a71be2265cb81fa310
Tangmengqi428 <874381226@qq.com>
2021-08-06 20:14:10 +0800

    can reply in comment

--- forum/views.py
+++ forum/views.py
@@ -18,43 +18,45 @@ def forumList(request):
         return render(request, 'forum/forum_list.html', kwargs)
     elif request.method=="POST":
         form=ForumForm(data=request.POST)
-        if form.is_valid():
-            forum=Forum()
-            forum.title=request.POST.get('title')
-            forum.author=request.user
-            #forum.created_time=request.POST.get('created_time')
-            forum.text=request.POST.get('text')
-            forum.save()
-            return HttpResponseRedirect(reverse("forum:forumList"))
+        #if form.is_valid():
+        forum=Forum()
+        forum.title=request.POST.get('title')
+        forum.author=request.user
+        #forum.created_time=request.POST.get('created_time')
+        forum.text=request.POST.get('text')
+        forum.save()
+        return HttpResponseRedirect(reverse("forum:forumList"))
     return render(request, 'forum/forum_list.html', locals())
 
 
 
-def post_forum(request,parent_forum_id):
+def post_forum(request,forum_id,parent_forum_id=None):
+    forum=get_object_or_404(Forum,id=forum_id)
     if request.method == 'POST':
-        forum = ForumForm(request.POST)
+        form = ForumForm(request.POST)
 
-        if forum.is_valid():
-            new_forum = forum.save(commit=False)
-            new_forum.author = request.author
+        #if forum.is_valid():
+        new_forum = form.save(commit=False)
+        new_forum.forum=forum
+        new_forum.author = request.author
 
-            # second
-            if parent_forum_id:
-                parent_forum = Forum.objects.get(id=parent_forum_id)
-                # 如果层级超过二级，则自动转为二级
-                new_forum.parent_id = parent_forum.get_root().id
-                # 被回复人
-                new_forum.reply_to = parent_forum.author
-                new_forum.save()
-                return HttpResponse("200 OK")
+        # second
+        if parent_forum_id:
+            parent_forum = Forum.objects.get(id=parent_forum_id)
+            # 如果层级超过二级，则自动转为二级
+            new_forum.parent_id = parent_forum.get_root().id
+            # 被回复人
+            new_forum.reply_to = parent_forum.author
             new_forum.save()
-            return HttpResponseRedirect(reverse("forum:forumList"))  # redirect(story)
+            return HttpResponse("200 OK")
+        new_forum.save()
+        return HttpResponseRedirect(reverse("forum:forumList"))  # redirect(story)
 
-        else:
-            return HttpResponse('There is something wrong with this form. Please fill it out again. ')
+        '''else:
+                    return HttpResponse('There is something wrong with this form. Please fill it out again. ')'''
 
     elif request.method == 'GET':
         forumform = ForumForm()
         forum=Forum.objects.get(id=parent_forum_id)
-        kwargs = {"forumform": forumform,"forum":forum}
-        return render(request, 'forum/forum_reply.html', kwargs)
+        #kwargs = {"forumform": forumform,"forum":forum}
+        return render(request, 'forum/forum_reply.html', locals())
