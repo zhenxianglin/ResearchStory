a281bb07dfa93f6dcbdd4556be68dda9e7194d36
Hankin72 <tracyguoji@gmail.com>
2021-08-08 03:42:30 +0800

    aaaa

--- Videos/views.py
+++ Videos/views.py
@@ -82,11 +82,11 @@ def new_video(request):
         form = NewVideoForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             new_form = form.save(commit=False)
-            new_form.uploader  = request.user
+            new_form.uploader = request.user
             new_form.save()
             return HttpResponseRedirect(reverse("Videos:video_list"))
     context = {"form": form,
-               'classification' : classification,
+               'classification': classification,
                }
     return render(request, 'Videos/new_video.html', context)
 
@@ -98,20 +98,25 @@ def edit_video(request, video_id):
     #  保护页面
     if video.uploader != request.user:
         raise Http404
-    if request.method != "POST":
-        form = NewVideoForm(instance=video)
-    else:
-        form = NewVideoForm(instance=video, data=request.POST,files=request.FILES)
-        #
+    if request.method == "POST":
+        form = NewVideoForm(instance=video, data=request.POST, files=request.FILES)
         if form.is_valid():
-            form.save()
-        return HttpResponseRedirect(reverse('Videos:video_detail', args=[video_id]))
-    context = {'video': video,
-               'form': form,
-               'classification': classification,
-               }
-    return render(request, "Videos/edit_video.html", context)
+            video.title = request.POST['title']
+            video.desc = request.POST['desc']
+            video.url = request.POST['url']
 
+            if 'file' in request.FILES:
+                video.file = request.POST['files']
+
+            video.save()
+            # return HttpResponse("edit successful")
+            return HttpResponseRedirect(reverse("Videos:video_detail", args=[video_id]))
+        else:
+            return HttpResponse("<h1>The content of the form is incorrect, please fill in again.</h1>")
+    else:
+        form = NewVideoForm(instance=video)
+        context = {'video':video, 'classification':classification, 'form':form}
+        return render(request, "Videos/edit_video.html", context)
 
 @login_required
 def video_delete(request, video_id):
