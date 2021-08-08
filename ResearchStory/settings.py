2cc38c1ccdf00b23e8713e930371410b1a3bf9f2
Hankin72 <tracyguoji@gmail.com>
2021-08-08 16:43:49 +0800

    sssaaaaaaaaa

--- ResearchStory/settings.py
+++ ResearchStory/settings.py
@@ -197,6 +197,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
