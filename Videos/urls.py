a06c94d829dbad8f0443ed599809654a35124499
Hankin72 <tracyguoji@gmail.com>
2021-08-08 18:43:21 +0800

    assssssssssssssssssssssaassss

--- Videos/urls.py
+++ Videos/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,re_path
+from django.urls import path, re_path
 from Videos import views
 from django.views.static import serve
 from django.conf import settings
@@ -11,9 +11,9 @@ urlpatterns = [
 
     path('video_list/post_comment/<int:video_id>/', views.post_video_comment, name='video_comment'),
 
-    path('new_video/', views.new_video, name = 'new_video'),
+    path('new_video/', views.new_video, name='new_video'),
 
-    path("edit_video/<int:video_id>/",views.edit_video, name = 'edit_video'),
+    path("edit_video/<int:video_id>/", views.edit_video, name='edit_video'),
 
     path(r'video_delete=<int:video_id>', views.video_delete, name="video_delete"),
 
