94d7a8f97ca5b38633ba27bbcba4c2332847819f
Hankin72 <tracyguoji@gmail.com>
2021-08-09 04:09:40 +0800

    11

--- ResearchStory/wsgi.py
+++ ResearchStory/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# from dj_static import Cling
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResearchStory.settings')
 
-application = get_wsgi_application()
-# application = Cling(get_wsgi_application())
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
