df788deae6847eb4c4423b20d0441ee3f55b6cf9
Hankin72 <tracyguoji@gmail.com>
2021-08-09 02:32:53 +0800

    111111

--- Users/views.py
+++ Users/views.py
@@ -72,7 +72,8 @@ def profile_edit(request, user_id):
                 profile.avatar = profile_cd["avatar"]
 
             profile.save()
-            return redirect("Users:edit", id=user_id)
+            return HttpResponseRedirect(reverse("Users:edit", args=[user_id]))
+            # return redirect("Users:edit", id=user_id)
         else:
             return HttpResponse("The registration form is entered incorrectly. Please re-enter~")
 
